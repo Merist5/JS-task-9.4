daada8ec7a029dbe76693c1812d34455cff15722
Joanna Urbiak <joanna.urbiak@o2.pl>
2018-05-06 21:08:07 +0200

    Change script.js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+drawTree(11);
+
+function drawTree(treeHeight) {
+    for (var i = 0; i <= treeHeight; i++) {
+        var star = '';
+        for (var j = 0; j <= i; j++) {
+            star += '*';
+        }
+        console.log(star);
+        
+    }
+}
